585d283d9799b623326292266c06779307780972
hoang.truong.minh <hoang.truong.minh@framgia.com>
2017-02-03 18:04:51 +0700

    init project

---
+++ CapyApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CapyApp.Startup))]
+namespace CapyApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
